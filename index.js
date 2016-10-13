7d1d835d5a52862ea58f722e73786bd00fa7711c
dry10124 <daniel.ryhle@sigma.se>
2016-10-13 10:00:39 +0200

    adjusted for proxytrust
    Change-Id: I7ca2b23981f818816586974e83f2b8e789b1e590

--- index.js
+++ index.js
@@ -3,20 +3,28 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
+
+//https://expressjs.com/en/guide/behind-proxies.html
+app.set('trust proxy', true);
+
 app.use(function (req, res, next) {
   var nodeSSPI = require('node-sspi');
   var nodeSSPIObj = new nodeSSPI({
     retrieveGroups: true,
-    useSSPI: true,
-    perRequestAuth: true,
+    useSSPI: false,
+    perRequestAuth: false,
     authoritative: true
   });
   nodeSSPIObj.authenticate(req, res, function(err){
     res.finished || next();
   });
 });
+
+
 app.use(function (req, res, next) {
   var out = 'Hello ' + req.connection.user + '! You belong to following groups:<br/><ul>';
+  out += '<li>remoteAddress: ' + req.connection.remoteAddress + '</li>'
+  out += '<li>forwarded-for: ' + req.headers['x-forwarded-for'] + '</li>';
   if (req.connection.userGroups) {
     for (var i in req.connection.userGroups) {
       out += '<li>'+ req.connection.userGroups[i] + '</li><br/>\n';
